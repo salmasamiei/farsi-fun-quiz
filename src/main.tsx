2f2129c8e2458897068fb1c62b7fabdfd6bb3bd2
Salma <salma.samiei@gmail.com>
2025-07-11 16:09:52 +0200

    Add files via upload

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import FarsiFunQuiz from './components/FarsiFunQuiz'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <FarsiFunQuiz />
+  </React.StrictMode>,
+)
